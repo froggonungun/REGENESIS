89796ba51ddcbce0e733607879a17083f58da868
JGERS1 <sviatogor1207@gmail.com>
2026-07-29 21:48:15 +0300

    very small fix

--- src/main/java/ru/jgers/regenesis/event/PlayerEventHandler.java
+++ src/main/java/ru/jgers/regenesis/event/PlayerEventHandler.java
@@ -11,7 +11,6 @@ import net.minecraft.world.entity.RelativeMovement;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
